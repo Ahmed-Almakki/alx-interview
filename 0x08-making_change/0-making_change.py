443279ee4825f7e9294278e544f3c5c618b2f4b1
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-28 22:03:12 +0000

    least number of coins

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""making chages"""
+
+
+def making_changes(coins, total, count):
+    if total < 0:
+        return -1
+    if total == 0:
+        return 0
+    tot = [total - i for i in coins if (total - i) >= 0]
+    if len(tot) == 0:
+        return -1
+    total = min(tot)
+    count += making_changes(coins, total, count)
+    if count == 0:
+        count = -1
+    return count
+
+
+def makeChange(coins, total):
+    if total == 0:
+        return 0
+    return making_changes(coins, total, count=1)
